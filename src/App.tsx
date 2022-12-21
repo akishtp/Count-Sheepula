42fdaf74519090e5336e08cd00e9670fda687f8c
akishtp <makishtp@gmail.com>
2022-12-21 14:15:21 +0530

    iphone ̊ lockscreen🔒

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,64 @@
-import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
 
 function App(): JSX.Element {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Count Sheepula</p>
-        <span>
-          I'm <code>Baa</code>cula
-        </span>
-      </header>
+      <div className="top-side"></div>
+      <div className="bottom-side">
+        <input type="number" />
+        <div className="button-row">
+          <button>
+            <span>1</span>
+          </button>
+          <button>
+            <span>2</span>
+            <p>ABC</p>
+          </button>
+          <button>
+            <span>3</span>
+            <p>DEF</p>
+          </button>
+        </div>
+        <div className="button-row">
+          <button>
+            <span>4</span>
+            <p>GHI</p>
+          </button>
+          <button>
+            <span>5</span>
+            <p>JKL</p>
+          </button>
+          <button>
+            <span>6</span>
+            <p>MNO</p>
+          </button>
+        </div>
+        <div className="button-row">
+          <button>
+            <span>7</span>
+            <p>PQRS</p>
+          </button>
+          <button>
+            <span>8</span>
+            <p>TUV</p>
+          </button>
+          <button>
+            <span>9</span>
+            <p>WXYZ</p>
+          </button>
+        </div>
+        <div className="button-row">
+          <button>
+            <span>🔙</span>
+          </button>
+          <button>
+            <span>0</span>
+          </button>
+          <button>
+            <span>✔️</span>
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
