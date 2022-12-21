9372ced60b43322a894f78ecba19b4bf01effd95
akishtp <makishtp@gmail.com>
2022-12-21 13:15:41 +0530

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
-function App() {
+function App(): JSX.Element {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>Count Sheepula</p>
+        <span>
+          I'm <code>Baa</code>cula
+        </span>
       </header>
     </div>
   );
