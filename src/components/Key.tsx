a31755b4e9b2b1c7dac55ee4dd5c442eda01ed9a
akishtp <makishtp@gmail.com>
2022-12-30 17:30:48 +0530

    error handling

--- src/components/Key.tsx
+++ src/components/Key.tsx
@@ -1,14 +1,22 @@
 import React from "react";
+import { useSelector } from "react-redux";
 import { useAppDispatch } from "../app/hooks";
-import { addKeyPress } from "../features/sheepCounterSlice";
+import { RootState } from "../app/store";
+import { addKeyPress, resetSheepValue } from "../features/sheepCounterSlice";
 
 interface KeyProps {
   value: string;
 }
 const Key: React.FC<KeyProps> = ({ value }) => {
+  let sheepValue = useSelector((state: RootState) => state.counter.value);
+
   const dispatch = useAppDispatch();
   const handleKeyClick = () => {
-    dispatch(addKeyPress(value));
+    if (sheepValue.length < 9) {
+      dispatch(addKeyPress(value));
+    } else {
+      resetSheepValue();
+    }
   };
   return (
     <button className="key" onClick={() => handleKeyClick()}>
--- src/features/sheepCounterSlice.ts
+++ src/features/sheepCounterSlice.ts
@@ -3,11 +3,13 @@ import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 export interface sheepValueState {
   value: string;
   status: "idle" | "loading" | "failed";
+  error: null | string;
 }
 
 const initialState: sheepValueState = {
   value: "",
   status: "idle",
+  error: null,
 };
 
 export const sheepCounterSlice = createSlice({
@@ -20,8 +22,12 @@ export const sheepCounterSlice = createSlice({
     removeKey: (state) => {
       state.value = state.value.substring(0, state.value.length - 1);
     },
+    resetSheepValue: (state) => {
+      state.value = "";
+    },
   },
 });
-export const { addKeyPress, removeKey } = sheepCounterSlice.actions;
+export const { addKeyPress, removeKey, resetSheepValue } =
+  sheepCounterSlice.actions;
 
 export default sheepCounterSlice.reducer;
